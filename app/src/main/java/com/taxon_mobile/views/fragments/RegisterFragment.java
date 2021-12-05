351db49a43a471b4e6be3b3ea11ade2ca9bce0e9
ssabrut <michael.gunawan2002@gmail.com>
2021-12-05 20:35:37 +0700

    Add register feature

--- app/src/main/java/com/taxon_mobile/views/fragments/RegisterFragment.java
+++ app/src/main/java/com/taxon_mobile/views/fragments/RegisterFragment.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.taxon_mobile.R;
+import com.taxon_mobile.models.RegisterResponse;
+import com.taxon_mobile.viewmodels.AuthViewModel;
 
 public class RegisterFragment extends Fragment {
 
@@ -24,6 +29,7 @@ public class RegisterFragment extends Fragment {
     private Toolbar register_toolbar;
     private TextInputLayout register_input_name, register_input_username, register_input_email, register_input_password;
     private Button register_btn;
+    private AuthViewModel viewModel;
 
     public RegisterFragment() {
     }
@@ -55,11 +61,29 @@ public class RegisterFragment extends Fragment {
         register_input_email = view.findViewById(R.id.register_input_email);
         register_input_password = view.findViewById(R.id.register_input_password);
         register_btn = view.findViewById(R.id.register_btn);
+        viewModel = new ViewModelProvider(this).get(AuthViewModel.class);
 
         register_toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Navigation.findNavController(view).navigate(R.id.action_registerFragment_to_loginFragment);
+            }
+        });
+
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = register_input_name.getEditText().getText().toString().trim();
+                String username = register_input_username.getEditText().getText().toString().trim();
+                String email = register_input_email.getEditText().getText().toString().trim();
+                String password = register_input_password.getEditText().getText().toString().trim();
+                viewModel.register(name, username, email, password);
+                viewModel.getRegisterDetails().observe(getViewLifecycleOwner(), new Observer<RegisterResponse>() {
+                    @Override
+                    public void onChanged(RegisterResponse registerResponse) {
+                        Toast.makeText(getContext(), registerResponse.getToken(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
