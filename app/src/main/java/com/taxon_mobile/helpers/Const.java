60ac897d001d8bebd79fc2117eb3626dc7f3e3c5
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2021-12-05 18:40:16 +0700

    Change BASE_URL to production server

--- app/src/main/java/com/taxon_mobile/helpers/Const.java
+++ app/src/main/java/com/taxon_mobile/helpers/Const.java
@@ -1,5 +1,5 @@
 package com.taxon_mobile.helpers;
 
 public class Const {
-    public static final String BASE_URL = "http://192.168.208.1/taxonomy-game-web/public/api/";
+    public static final String BASE_URL = "http://https://taxon-game-web.herokuapp.com/api/";
 }
