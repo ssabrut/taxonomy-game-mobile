f2608bb76bb1c391c866f85fe4ffd9a8641419d6
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2021-12-05 18:51:28 +0700

    Fix BASE_URL

--- app/src/main/java/com/taxon_mobile/helpers/Const.java
+++ app/src/main/java/com/taxon_mobile/helpers/Const.java
@@ -1,5 +1,5 @@
 package com.taxon_mobile.helpers;
 
 public class Const {
-    public static final String BASE_URL = "http://https://taxon-game-web.herokuapp.com/api/";
+    public static final String BASE_URL = "https://taxon-game-web.herokuapp.com/api/";
 }
