ed12ffdd07cd74f007062dcaa61080a47ffd2879
ssabrut <michael.gunawan2002@gmail.com>
2021-12-06 09:34:43 +0700

    Add clicking feature

--- app/src/main/java/com/taxon_mobile/views/fragments/MainFragment.java
+++ app/src/main/java/com/taxon_mobile/views/fragments/MainFragment.java
@@ -2,43 +2,30 @@ package com.taxon_mobile.views.fragments;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.taxon_mobile.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MainFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
+    private ConstraintLayout main_canvas;
+    private TextView main_user_currency, main_user_click_power;
+
     public MainFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MainFragment newInstance(String param1, String param2) {
         MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
@@ -58,9 +45,22 @@ public class MainFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        main_canvas = view.findViewById(R.id.main_canvas);
+        main_user_currency = view.findViewById(R.id.main_user_currency);
+        main_user_click_power = view.findViewById(R.id.main_user_click_power);
+
+        main_canvas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int currency = Integer.parseInt(main_user_currency.getText().toString());
+                int clickPower = Integer.parseInt(main_user_click_power.getText().toString());
+                currency += clickPower;
+                main_user_currency.setText(String.valueOf(currency));
+            }
+        });
+
+        return view;
     }
 }
