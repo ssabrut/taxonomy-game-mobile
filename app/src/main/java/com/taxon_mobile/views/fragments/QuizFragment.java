be4e132389c21e2927e99108ae3d88df1d3a80b0
ssabrut <michael.gunawan2002@gmail.com>
2022-01-07 19:39:29 +0700

    Add quiz on finished feedback

--- app/src/main/java/com/taxon_mobile/views/fragments/QuizFragment.java
+++ app/src/main/java/com/taxon_mobile/views/fragments/QuizFragment.java
@@ -1,9 +1,13 @@
 package com.taxon_mobile.views.fragments;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -11,11 +15,14 @@ import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputLayout;
@@ -28,7 +35,9 @@ import com.taxon_mobile.viewmodels.UserStatViewModel;
 import com.taxon_mobile.views.MainActivity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class QuizFragment extends Fragment implements IOnBackPressed {
 
@@ -41,9 +50,15 @@ public class QuizFragment extends Fragment implements IOnBackPressed {
     private QuizViewModel viewModel;
     public static Button quiz_submit_btn;
     private RecyclerView quiz_rv;
+    private Dialog quizFeedbackDialog;
+    private LinearLayout quiz_feedback_layout;
+    private Button quiz_feedback_finish_btn;
+    private View foo;
 
     private List<String> answers;
     private List<String> keys;
+    private List<String> questions;
+    private int correctAnswer = 0;
 
     public QuizFragment() {
         // Required empty public constructor
@@ -71,14 +86,18 @@ public class QuizFragment extends Fragment implements IOnBackPressed {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_quiz, container, false);
         QuizAdapter adapter = new QuizAdapter(getActivity().getApplicationContext());
+        foo = view;
+        quizFeedbackDialog = new Dialog(getActivity());
         quiz_rv = view.findViewById(R.id.quiz_rv);
         quiz_submit_btn = view.findViewById(R.id.quiz_submit_btn);
         answers = new ArrayList<>();
         keys = new ArrayList<>();
+        questions = new ArrayList<>();
         MainActivity.counter = 0;
         viewModel = new ViewModelProvider(this).get(QuizViewModel.class);
         viewModel.quiz("Bearer " + MainActivity.token);
         viewModel.getQuizDetail().observe(getViewLifecycleOwner(), showQuiz);
+        quizFeedbackDialog();
 
         quiz_submit_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,19 +123,56 @@ public class QuizFragment extends Fragment implements IOnBackPressed {
                 for (int i = 0; i < keys.size(); i++) {
                     if (answers.get(i).equalsIgnoreCase(keys.get(i))) {
                         MainActivity.point += 10;
+                        correctAnswer++;
+                    } else {
+                        TextView textView = new TextView(getActivity().getApplicationContext());
+                        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+                                ViewGroup.LayoutParams.WRAP_CONTENT,
+                                ViewGroup.LayoutParams.WRAP_CONTENT
+                        );
+
+                        if (i == 0) {
+                            layoutParams.setMargins(32, 64, 32, 0);
+                        } else {
+                            layoutParams.setMargins(32, 16, 32, 0);
+                        }
+
+                        textView.setLayoutParams(layoutParams);
+                        textView.setText(questions.get(i));
+                        textView.setTextColor(getResources().getColor(R.color.dark_black));
+                        quiz_feedback_layout.addView(textView);
                     }
                 }
 
-                UserStatViewModel statViewModel = new ViewModelProvider(getActivity()).get(UserStatViewModel.class);
-                statViewModel.saveUserStat("Bearer " + MainActivity.token, MainActivity.power, MainActivity.evo, MainActivity.dna, MainActivity.point);
-                SharedPreferences sp = getContext().getSharedPreferences("UserStat", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sp.edit();
-                editor.putInt("power", MainActivity.power);
-                editor.putInt("evo", MainActivity.evo);
-                editor.putInt("dna", MainActivity.dna);
-                editor.putInt("point", MainActivity.point);
-                editor.commit();
-                Navigation.findNavController(view).navigate(R.id.action_quizFragment_to_mainFragment);
+                TextView quiz_feedback_finish_btn = new TextView(getActivity().getApplicationContext());
+                quiz_feedback_finish_btn.setText(getResources().getText(R.string.finish_quiz));
+                quiz_feedback_finish_btn.setGravity(Gravity.END | Gravity.BOTTOM);
+                quiz_feedback_finish_btn.setTextSize(20f);
+                quiz_feedback_finish_btn.setPadding(0, 32, 65, 56);
+                quiz_feedback_layout.addView(quiz_feedback_finish_btn);
+
+                quiz_feedback_finish_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        UserStatViewModel statViewModel = new ViewModelProvider(getActivity()).get(UserStatViewModel.class);
+                        statViewModel.saveUserStat("Bearer " + MainActivity.token, MainActivity.power, MainActivity.evo, MainActivity.dna, MainActivity.point);
+                        SharedPreferences sp = getContext().getSharedPreferences("UserStat", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sp.edit();
+                        editor.putInt("power", MainActivity.power);
+                        editor.putInt("evo", MainActivity.evo);
+                        editor.putInt("dna", MainActivity.dna);
+                        editor.putInt("point", MainActivity.point);
+                        editor.commit();
+                        Navigation.findNavController(foo).navigate(R.id.action_quizFragment_to_mainFragment);
+                        quizFeedbackDialog.dismiss();
+                    }
+                });
+
+                if (questions.size() > 0) {
+                    questions.clear();
+                }
+
+                quizFeedbackDialog.show();
             }
         });
 
@@ -127,6 +183,10 @@ public class QuizFragment extends Fragment implements IOnBackPressed {
         @Override
         public void onChanged(List<Quiz.Quizzes> quizzes) {
             quiz_rv.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+            for (int i = 0; i < quizzes.size(); i++) {
+                questions.add(quizzes.get(i).getQuestion());
+            }
+
             QuizAdapter adapter = new QuizAdapter(getActivity().getApplicationContext());
             adapter.setListQuiz(quizzes);
             quiz_rv.setAdapter(adapter);
@@ -143,5 +203,16 @@ public class QuizFragment extends Fragment implements IOnBackPressed {
     @Override
     public boolean onBackPresed() {
         return true;
+    }
+
+    private void quizFeedbackDialog() {
+        quizFeedbackDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        quizFeedbackDialog.setContentView(R.layout.quiz_feedback_dialog);
+        quizFeedbackDialog.getWindow().setLayout(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        quizFeedbackDialog.setCancelable(false);
+        quiz_feedback_layout = quizFeedbackDialog.findViewById(R.id.quiz_feedback_layout);
     }
 }
